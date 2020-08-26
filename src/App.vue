efb126b0218b71087e889a0e4d5d666667cedc4c
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-08-26 20:20:49 +0100

    final version

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
   <div>
     <h1>Rick and Morty Characters</h1>
     <main>
-      <character-list :characters="characters"></character-list>
+      <label for="char_select">Select a Character:</label>
+      <select id="char_select" v-model="selectedCharacter">
+      <option disabled value>Select a Character</option>
+      <option v-for="(character, index) in characters" :key="index" :value="character">{{character.name}}</option>
+      </select>
+      <!-- <character-list :characters="characters"></character-list> -->
       <char-details :selectedCharacter='selectedCharacter'></char-details>
     </main>
   </div>
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,14 +1,8 @@
 <template>
   <div>
-    <label for="char_select">Select a Character:</label>
-    <select id="char_select" v-on:change="handleChange" v-model="selectedCharacter">
-      <option disabled value>Select a Character</option>
-      <option
-        v-for="(character, index) in characters"
-        :key="index"
-        :value="character"
-      >{{character.name}}</option>
-    </select>
+    <ul>
+        <list-item v-for="(character, index) in characters" :character="character" :key="index"></list-item>
+    </ul>
   </div>
 </template>
 
@@ -18,19 +12,9 @@ import CharacterListItem from "./CharacterListItem.vue";
 
 export default {
   name: "character-list",
-  data(){
-      return {
-        selectedCharacter: null
-      }
-  },
   props: ["characters"],
   components: {
     "char-list-item": CharacterListItem,
-  },
-  methods: {
-        handleChange() {
-            eventBus.$emit('char-selected', this.selectedCharacter)
-        }
   }
 };
 </script>
